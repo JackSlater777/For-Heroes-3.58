a7408b0874a4c18f1121185260664345e8fe7e99
Ivan Karazanov <dualking1991@gmail.com>
2023-09-02 13:00:51 +0300

    delete unused files

---
+++ example_injecting.py
@@ -0,0 +1,10 @@
+from pymem import Pymem
+
+pm = Pymem('notepad.exe')
+print('Process id: %s' % pm.process_id)
+address = pm.allocate(10)
+print('Allocated address: %s' % address)
+pm.write_int(address, 1337)
+value = pm.read_int(address)
+print('Allocated value: %s' % value)
+pm.free(address)
